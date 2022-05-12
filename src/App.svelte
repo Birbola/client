9798283462c686ee926b4d87ecb348955138df25
the1ASTRO <abulovshoxruh8@gmail.com>
2022-05-12 21:22:15 +0500

    home

---
+++ src/App.svelte
@@ -0,0 +1,49 @@
+<script lang="ts">
+	import Home from "./pages/home.svelte";
+	import { Router, Route, Link} from "svelte-navigator"
+</script>
+
+<Router>
+<main class="w-100 p-0">
+	<div class="navbar bg-base-100 w-100 text-white shadow-lg shadow-green-500/50">
+		<div class="flex-none">
+			<div class="dropdown">
+				<label tabindex="0" class="btn btn-ghost btn-circle">
+					<svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
+				</label>
+				<ul tabindex="0" class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+					<li><Link to="/">Homepage</Link></li>
+					<li><a>About</a></li>
+					<li><a>Projects</a></li>
+				</ul>
+			</div>
+		</div>
+		<div class="flex-1">
+			<a class="nav-link btn btn-ghost normal-case text-xl">Javohir Otanazarov</a>
+		</div>
+		<div class="flex-none">
+      <label tabindex="0" class="btn btn-ghost btn-circle avatar">
+        <div class="w-10 rounded-full">
+          <img src="https://images.pexels.com/photos/2379005/pexels-photo-2379005.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" />
+        </div>
+      </label>
+		</div>
+	</div>
+</main>
+
+<Route path="/" component={Home}></Route>
+</Router>
+
+<style>
+	.navbar{
+		padding: 5px 15px;
+	}
+	@media (max-width:450px) {
+		.navbar{
+			padding: 5px 10px;
+		}
+		.nav-link{
+			padding: 0;
+		}
+	}
+</style>
---
+++ src/pages/home.svelte
@@ -0,0 +1,234 @@
+<div class="home-main">
+  <div class="pubg flex mt-24 justify-between items-center px-10">
+    <div class="pubg-content flex flex-col gap-3" data-aos="zoom-in-left">
+      <h1 class="pubg-title text-5xl">Hello, I'm Javohir</h1>
+      <p class="pubg-desc text-5xl">I'm Backen developer and Pubg player</p>
+    </div>
+    
+    <div class=" grid h-100 flex-grow card bg-base-150 rounded-box place-items-center" data-aos="zoom-in-right">
+      <img class="pubg-img" src="https://wallpaperaccess.com/full/2540626.png" alt="">
+    </div>
+  </div>
+</div>
+
+<div class="home-second">
+  <div class="pubg-second flex mt-10 w-100 justify-between items-center px-10 ">
+    <div data-aos="zoom-in-right" class="second-img">
+      <img class="pubg-second-img" src="https://www.pngplay.com/wp-content/uploads/11/PUBG-PNG-HD-Free-File-Download.png" alt="">
+    </div>
+
+    <div data-aos="zoom-in-left" class="second-text pr-24 w-100">
+      <h1 class="text-5xl second-texts">I want to be a great programmer</h1>
+      <p class="text-5xl second-texts">I will definitely achieve that!!!</p>
+    </div>
+  </div>
+</div>
+
+<div class="home-last bg-primary mt-10" data-aos="zoom-in">
+  <footer class="footer p-10">
+
+    <div class="mx-auto">
+      <i class="smile fa-solid fa-face-smile"></i>
+      <p class="tel">Bog'lanish uchun:<br>+99897-364-51-59</p>
+    </div> 
+
+    <div class="mx-auto">
+      <span class="footer-title">Social</span> 
+      <div class="grid grid-flow-col gap-4">
+        <a href=""><i class="text-white fa-brands fa-telegram home-ico"></i></a>
+        <a href=""><i class="text-white fa-solid fa-envelope home-ico"></i></a>
+      </div>
+    </div>
+
+  </footer>
+</div>
+
+<!-- home main -->
+<style>
+  .pubg-img{
+    width: 650px;
+    height: 808px;
+  }
+
+  .home-ico{
+    width: 24px;
+    height: 24px;
+  }
+
+  .smile{
+    width: 80px;
+    height: 80px;
+    color: yellow;
+  }
+
+  @media (max-width:1600px){
+    .pubg{
+      padding: 0 40px;
+    }
+
+    .pubg-img{
+      height: 700px;
+    }
+  }
+
+  @media (max-width:1440px){
+    .pubg{
+      padding: 0 40px;
+    }
+
+    .pubg-img{
+      width: 520px;
+      height: 700px;
+    }
+
+    .second-texts{
+      font-size: 50px;
+    }
+
+    .pubg-second-img{
+      width: 520px;
+      height: 660px;
+    }
+  }
+
+  @media (max-width:1300px){
+    .pubg{
+      padding: 0 40px;
+    }
+
+    .pubg-img{
+      width: 500px;
+      height: 500px;
+    }
+
+    .second-texts{
+      font-size: 40px;
+    }
+
+    .second-text{
+      padding-right: 0;
+    }
+
+    .pubg-second-img{
+      width: 480px;
+      height: 600px;
+    }
+  }
+
+  @media (max-width:800px) {
+    .pubg{
+      flex-direction: column;
+      gap: 30px;
+    }
+    
+    .pubg-content{
+      text-align: center;
+    }
+
+    .pubg-img{
+      width: 460px;
+      height: 608px;
+    }
+
+    .pubg-second{
+      flex-direction: column-reverse;
+      gap: 20px;
+    }
+    
+    .second-text{
+      text-align: center;
+    }
+  }
+
+  @media (max-width:500px) {
+    .pubg{
+      flex-direction: column;
+      margin-top: 50px;
+      gap: 20px;
+    }
+
+    .pubg-content{
+      text-align: center;
+    }
+
+    .pubg-title{
+      font-size: 35px;
+    }
+
+    .pubg-desc{
+      font-size: 35px;
+    }
+
+    .pubg-img{
+      width: 400px;
+      height: 520px;
+    }
+
+    .pubg-second{
+      padding: 0 10px;
+    }
+
+    .pubg-second-img{
+      width: 460px;
+      height: 520px;
+    }
+
+    .second-texts{
+      font-size: 30px;
+    }
+
+    .smile{
+      margin: 0 auto;
+    }
+  }
+
+  @media (max-width:400px) {
+    .pubg{
+      flex-direction: column;
+      gap: 15px;
+    }
+    
+    .pubg-content{
+      text-align: center;
+    }
+
+    .pubg-title{
+      font-size: 30px;
+    }
+
+    .pubg-desc{
+      font-size: 30px;
+    }
+
+    .pubg-img{
+      width: 300px;
+      height: 400px;
+    }
+
+    .tel{
+      text-align: center;
+    }
+  }
+
+  @media (max-width:350px) {
+    .pubg-title{
+      font-size: 25px;
+    }
+
+    .pubg-desc{
+      font-size: 25px;
+    }
+
+    .pubg-img{
+      height: 360px;
+    }
+
+    .second-texts{
+      font-size: 25px;
+    }
+
+    .pubg-second-img{
+      height: 400px;
+    }
+  }
+</style>
